bdd74da9165cca65e0719cb55fcf20b158be5db3
regullus <rabellomc@gmail.com>
2024-06-07 12:45:22 -0300

    v.2.3.6

---
+++ Univer/Application/Core/StoredProcedures/Universol/spC_UsuariosPaises.sql
@@ -0,0 +1,107 @@
+Use univerDev
+go
+If Exists (Select 'Sp' From sysobjects Where id = object_id('spC_UsuariosPaises'))
+   Drop Procedure spC_UsuariosPaises
+go
+
+Create PROCEDURE [dbo].[spC_UsuariosPaises]
+
+AS
+-- =============================================================================================
+-- Author.....: Adamastor
+-- Create date: 16/02/2018
+-- Description: Obtem os Avisos não lidos do usuario
+-- =============================================================================================
+BEGIN
+    --Necessario para o entity reconhecer retorno de select com tabela temporaria
+    Set FMTONLY OFF
+    Set nocount on
+
+    Declare @total int
+
+    Create Table #temp 
+    (
+        Total int,
+        PaisID int,
+        IdiomaID int,
+        Nome nvarchar(255),
+        Idioma nvarchar(255)
+    )
+    Insert Into
+        #temp
+    Select 
+        count(*) Total,
+        PaisID,
+        0,
+        '',
+        ''
+    From
+        Usuario.usuario
+    Group By
+        PaisID
+    Order By
+        Total desc
+
+    Update
+        tmp
+     Set
+        tmp.Nome = p.Nome,
+        tmp.IdiomaID = p.IdiomaID
+    From
+        #temp tmp,
+        Globalizacao.Pais p
+    Where
+        tmp.PaisID = p.ID
+
+    Update
+        tmp
+    Set
+        tmp.Idioma = i.Nome
+    From
+        #temp tmp,
+        Globalizacao.Idioma i
+    Where
+        tmp.IdiomaID = i.ID
+    
+    Update
+        tmp
+    Set
+        tmp.Idioma = i.Nome
+    From
+        #temp tmp,
+        Globalizacao.Idioma i
+    Where
+        tmp.IdiomaID = i.ID
+
+    Select 
+        @total=SUM(Total)
+    From
+        #temp
+
+    Insert Into 
+        #temp
+    Select
+        @total total,
+        0 PaisID,
+        0 IdiomaID,
+        'Total' Nome,
+        ''
+
+    Select
+        Total,
+        Nome,
+        Idioma
+    From
+        #temp
+    Order By
+        Total Desc
+   
+END  
+
+go
+   Grant Exec on spC_UsuariosPaises To public
+go
+
+Exec spC_UsuariosPaises 
+
+
--- Univer/Banco/Teste/QueryTeste.sql
+++ Univer/Banco/Teste/QueryTeste.sql
@@ -5,11 +5,11 @@ go
 --Delete Rede.TabuleiroUltimoAcesso
 
 /*
-Update Usuario.usuario set senha = 'YWbWJ3CI34EG1FB1eU6Ryg==' where id>2586
-Update Autenticacao set PasswordHash = 'AFdh14qX3cC2KR5OLsCnYPVunVMzwAKzr2Z0r8Z0+oRYezufFNVs+WM+y4tWXMsk4w==', SecurityStamp = '95c37d77-4bab-4b44-827f-b7a72a2a73db' where id>2586
+Update Usuario.usuario set senha = 'YWbWJ3CI34EG1FB1eU6Ryg==' where id>2001
+Update Autenticacao set PasswordHash = 'AFdh14qX3cC2KR5OLsCnYPVunVMzwAKzr2Z0r8Z0+oRYezufFNVs+WM+y4tWXMsk4w==', SecurityStamp = '95c37d77-4bab-4b44-827f-b7a72a2a73db' where id>2001
 */
-
---Select id,login, apelido, PatrocinadorDiretoID from usuario.usuario where id = 2584
+select * from Usuario.usuario
+--Select id,login, apelido, PatrocinadorDiretoID from usuario.usuario where id = 2579
 
 Declare @UsuarioID int, @BoardID int, @TabuleiroID int, @PatrocinadorID int, @MasterID int
 
